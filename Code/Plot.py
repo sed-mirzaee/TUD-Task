9ecc44551e2be61e66bfbd62160968f12523bc45
Sedigheh Mirzaei <38011631+sed-mirzaee@users.noreply.github.com>
2025-01-15 06:19:25 +0330

    Add files via upload

--- Code/Plot.py
+++ Code/Plot.py
@@ -3,45 +3,60 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Preparing data sets for age restrictions
-netflix_data = [0]*144 + [7]*322 + [13]*404 + [16]*151 + [18]*877
-disney_data = [0]*370 + [7]*278 + [13]*70 + [16]*4 + [18]*3
+netflix_data = {'(0-20)': 1.46, '(21-40)': 12.74, '(41-60)': 55.12, '(61-80)': 26.68, '(81-100)': 3.99}
 
 #Plot Age Restrictions on Netflix
 plt.figure(figsize=(5, 4))
-plt.hist(netflix_data, bins=[0, 7, 13, 16, 18, 20], edgecolor='black', alpha=0.7)
-plt.title('Histogram of Age Restrictions for movies on Netflix')
-plt.xlabel('Age Restriction')
-plt.ylabel('Frequency')
-plt.xticks([0, 7, 13, 16, 18])
+plt.bar(list(netflix_data.keys()), list(netflix_data.values()))
+plt.title('Relative Frequency for Rotten Tomatoes Score on Netflix')
+plt.xlabel('Rotten Tomatoes Score')
+plt.ylabel('Relative Frequency (percent)')
+plt.xticks(["(0-20)", "(21-40)", "(41-60)", "(61-80)", "(81-100)"])
 plt.show()
+
+#Preparing data sets for age restrictions
+disney_data =  {'0+': 51.03, '7+': 38.34, '13+': 9.66, '16+': 0.55, '18+': 0.41}
+netflix_data = {'0+': 7.59, '7+': 16.97, '13+': 21.29, '16+': 7.96, '18+': 46.21}
 
 #Plot Age Restrictions on Disney+
 plt.figure(figsize=(5, 4))
-plt.hist(disney_data, bins=[0, 7, 13, 16, 18, 20], edgecolor='black', alpha=0.7)
-plt.title('Histogram of Age Restrictions for movies on Disney+')
+plt.bar(list(disney_data.keys()), list(disney_data.values()))
+plt.title('(a) Relative Frequency for Age Restrictions on Disney+')
 plt.xlabel('Age Restriction')
-plt.ylabel('Frequency')
-plt.xticks([0, 7, 13, 16, 18])
+plt.ylabel('Relative Frequency (percent)')
+plt.xticks(["0+", "7+", "13+", "16+", "18+"])
+plt.show()
+
+#Plot Age Restrictions on Netflix
+plt.figure(figsize=(5, 4))
+plt.bar(list(netflix_data.keys()), list(netflix_data.values()))
+plt.title('(b) Relative Frequency for Age Restrictions on Netflix')
+plt.xlabel('Age Restriction')
+plt.ylabel('Relative Frequency (percent)')
+plt.xticks(["0+", "7+", "13+", "16+", "18+"])
 plt.show()
 
 #read from csv file
 dr = pd.read_csv("G:/Application/3/Data/DisneyRates.csv")
+nr = pd.read_csv("G:/Application/3/Data/NetflixRates.csv")
+
 #Plot Scores on Disney+
 plt.figure(figsize=(9, 4))
 plt.hist(dr.DisneyRate, bins=19,  edgecolor='black', alpha=0.7)
-plt.title('Histogram of Rotten Tomatoes score for movies on Disney+')
-plt.xlabel('Rotten Tomatoes score')
+plt.title('(a) Histogram of Rotten Tomatoes score for movies on Disney+')
+plt.xlabel('Rotten Tomatoes Score')
 plt.ylabel('Frequency')
 plt.xticks([10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])
 plt.show()
 
-#read from csv file
-nr = pd.read_csv("G:/Application/3/Data/NetflixRates.csv")
 #Plot Scores on Netflix
 plt.figure(figsize=(9, 4))
 plt.hist(nr.NetflixRate, bins=19,  edgecolor='black', alpha=0.7)
-plt.title('Histogram of Rotten Tomatoes score for movies on Netflix')
-plt.xlabel('Rotten Tomatoes score')
+plt.title('(b) Histogram of Rotten Tomatoes score for movies on Netflix')
+plt.xlabel('Rotten Tomatoes Score')
 plt.ylabel('Frequency')
 plt.xticks([10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])
 plt.show()
+
+
+
--- Code/Tests.py
+++ Code/Tests.py
@@ -2,17 +2,19 @@
 import pandas as pd
 import scipy.stats as stats
 
-
 #Question1: Is the age restriction for movies on Disney+ lower than for movies on Netflix?
-netflix_data = [0]*144 + [7]*322 + [13]*404 + [16]*151 + [18]*877
-disney_data = [0]*370 + [7]*278 + [13]*70 + [16]*4 + [18]*3
+disney_data = pd.read_csv("G:/Application/3/Data/DisneyAge.csv")
+netflix_data = pd.read_csv("G:/Application/3/Data/NetflixAge.csv")
 
-u_statistic, p_value = stats.mannwhitneyu(disney_data, netflix_data, alternative='less')
-print("Question1, Mann-Whitney U Test result: ", "U-Stat: ",u_statistic,"P-Value: ", p_value)
+u_value, p_value = stats.mannwhitneyu(disney_data.DisneyAge, netflix_data.NetflixAge, alternative='less')
+print("Question1: Is the age restriction for movies on Disney+ lower than for movies on Netflix?")
+print(" Mann-Whitney U Test result: ", f"U-Value: {u_value:.5f},     P-Value: {p_value:.5f}")
 
 #Question2: Is there a difference in Rotten Tomatoes Score for movies on those two platforms?
 dr = pd.read_csv("G:/Application/3/Data/DisneyRates.csv")
 nr = pd.read_csv("G:/Application/3/Data/NetflixRates.csv")
 
-u_statistic1, p_value1 = stats.ttest_ind(a=nr.NetflixRate, b=dr.DisneyRate, equal_var=True)
-print("Question2, T-Test result: ", "U-Stat: ",u_statistic1,"P-Value: ", p_value1)
+t_value, p_value = stats.ttest_ind(a=dr.DisneyRate, b=nr.NetflixRate, equal_var=False)
+print(f"\n\nQuestion2: Is there any differences in Rotten Tomatoes Score between the two platforms?")
+print(" T-Test result: ", f"T-Value: {t_value:.5f},     P-Value: {p_value:.5f}")
+
